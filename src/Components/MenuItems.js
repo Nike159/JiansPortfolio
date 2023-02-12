054250b422710d9ae54a85d1c3309657929c0d40
Nike159 <nikeblack159@gmail.com>
2023-02-11 20:56:58 -0600

    React Portfolio
    Learning React

---
+++ src/Components/MenuItems.js
@@ -0,0 +1,26 @@
+export const MenuItems = [
+    {
+        title : "About",
+        url : "/about",
+        cName : "nav-links",
+        icon : "fa-solid fa-user"
+    },
+    {
+        title : "Skills",
+        url : "/about",
+        cName : "nav-links",
+        icon : "fa-solid fa-books"
+    },
+    {
+        title : "Contact",
+        url : "/contact",
+        cName : "nav-links",
+        icon : "fa-light fa-address-book"
+    },
+    {
+        title : "Projects",
+        url : "/projects",
+        cName : "nav-links",
+        icon : "fa-solid fa-briefcase"
+    },
+]
