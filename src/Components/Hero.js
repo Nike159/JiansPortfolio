2ec76f5b62d23daf084ed18cbecd159faae5b9cc
Nike159 <108959165+Nike159@users.noreply.github.com>
2023-03-08 22:03:29 -0600

    Resume link
    link resume

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,28 +1,24 @@
 import "./HeroStyles.css";
 import {Link} from "react-router-dom";
 
-
-
-
-
 function Hero (props){
     return(
         <>
-       <div className={props.cName}>
-            <img alt="HeroImg" src={props.heroImg}/>
-            <div className="hero-text">
-                <h1>{props.title}</h1>
-                <p>{props.text}</p>
-                <div className="welcome">
-                    <Link className="welcomebtn" to="/">
-                        <button>Resume</button>
-                    </Link>
-                    <Link className="welcomebtn" to="/Projects">
-                        <button>Projects &#x2192;</button>
-                    </Link>
+            <div className={props.cName}>
+                <img alt="HeroImg" src={props.heroImg}/>
+                <div className="hero-text">
+                    <h1>{props.title}</h1>
+                    <p>{props.text}</p>
+                    <div className="welcome">
+                        <a className="welcomebtn" rel="noreferrer" href="https://nike159.github.io/Resume/" target="_blank">
+                            <button>Resume</button>
+                        </a>
+                        <Link className="welcomebtn" to="/Projects">
+                            <button>Projects &#x2192;</button>
+                        </Link>
+                    </div>
                 </div>
             </div>
-        </div>
         </>
     );
 }
